a0bd997d29b1d6869be541d5a4ef50bdedcaa289
Amansingh <singhamankr17@gmail.com>
2024-10-10 22:21:11 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParse());
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://class-notion.vercel.app/',
     credentials:true
 }))
 
